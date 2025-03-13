163d797664625a8597c480b7270ce9761dda6669
David Granado <davidjgranado@gmail.com>
2025-03-13 16:53:00 -0500

    Adjustment

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ export { default as EntityManager } from './entity-manager';
 export { default as EventBus } from './event-bus';
 export { default as ResourceManager } from './resource-manager';
 export { SystemBuilder };
-export default SimpleECS;
 export { Bundle, mergeBundles };
+export default SimpleECS;
