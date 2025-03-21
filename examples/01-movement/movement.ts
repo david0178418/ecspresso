f9148a0798a4dbbd7cdcb61a28c7c5252fc32617
David Granado <davidjgranado@gmail.com>
2025-03-21 14:32:36 -0500

    Begin fleshing out some examples

---
+++ examples/01-movement/movement.ts
@@ -0,0 +1,143 @@
+import { Application, Graphics, Sprite } from 'pixi.js';
+import ECSpresso from "../../src";
+
+interface Components {
+	sprite: Sprite;
+	position: {
+		x: number;
+		y: number;
+	};
+	velocity: {
+		x: number;
+		y: number;
+	};
+	radius: number;
+}
+
+interface Resources {
+	pixi: Application;
+}
+
+const ecs = new ECSpresso<Components, {}, Resources>();
+const pixi = new Application();
+
+await initializeGame();
+
+setupGameLogic();
+
+startGameLoop();
+
+async function initializeGame() {
+	// Setup Stage
+	await pixi.init({
+		background: '#1099bb',
+		resizeTo: window,
+	});
+
+	const canvasContainerEl = document
+		.createElement('div')
+		.appendChild(pixi.canvas);
+
+	document.body.appendChild(canvasContainerEl);
+
+	// Create ball entity
+	const ballEntity = ecs.entityManager.createEntity();
+	const ballRadius = 30;
+	const sprite = createCircleSprite(0x0000FF, pixi, ballRadius)
+	sprite.anchor.set(0.5, 0.5); // "position" will be relative to the center of the sprite
+
+	ecs.entityManager.addComponents(ballEntity, {
+		sprite,
+		radius: ballRadius,
+		position: {
+			x: pixi.screen.width / 2,
+			y: pixi.screen.height / 2
+		},
+		velocity: {
+			x: 300,
+			y: 250
+		}
+	});
+
+	pixi.stage.addChild(sprite);
+}
+
+function setupGameLogic() {
+
+	ecs
+	.addResource('pixi', pixi)
+	.addSystem('move-entities')
+	.addQuery('movingEntities', {
+		// Apply this logic only to entities with
+		// position, velocity, and radius components
+		with: ['position', 'velocity', 'radius'],
+	})
+	.setProcess((queries, deltaTimeMs, ecs) => {
+		const pixi = ecs.resourceManager.get('pixi');
+
+		for(const entity of queries.movingEntities) {
+			const { position, velocity, radius } = entity.components;
+
+			// Update position
+			position.x += velocity.x * deltaTimeMs;
+			position.y += velocity.y * deltaTimeMs;
+
+			// Bounce off screen edges
+			const maxX = pixi.screen.width - radius;
+			const maxY = pixi.screen.height - radius;
+			const minX = radius;
+			const minY = radius;
+
+			if (position.x > maxX) {
+				position.x = maxX;
+				velocity.x *= -1;
+			} else if (position.x < minX) {
+				position.x = minX;
+				velocity.x *= -1;
+			}
+
+			if (position.y > maxY) {
+				position.y = maxY;
+				velocity.y *= -1;
+			} else if (position.y < minY) {
+				position.y = minY;
+				velocity.y *= -1;
+			}
+		}
+	})
+	// Build system into the ECSpresso instance when finished defining it
+	.build()
+	// Access the ECSpresso instance for chaining.
+	.ecspresso
+	// Update sprite positions based on entity positions
+	.addSystem('update-sprite-position')
+	.addQuery('renderedEntities', {
+		with: ['sprite', 'position'],
+	})
+	.setProcess((queries) => {
+		for(const entity of queries.renderedEntities) {
+			entity.components.sprite.position.set(
+				entity.components.position.x,
+				entity.components.position.y
+			);
+		}
+	})
+	.build();
+}
+
+function startGameLoop() {
+	pixi.ticker.add(ticker => {
+		ecs.update(ticker.deltaMS / 1000);
+	});
+}
+
+
+function createCircleSprite(color: number, pixi: Application, radius: number): Sprite {
+	const texture = pixi.renderer.generateTexture(
+		new Graphics()
+			.circle(0, 0, radius)
+			.fill(color)
+	);
+
+	return new Sprite(texture);
+}
---
+++ examples/serve-examples.ts
@@ -0,0 +1,22 @@
+import { serve } from "bun";
+import home from './index.html';
+import basiceMovement from './01-movement/movement.html';
+import playerInput from './02-player-input/player-input.html';
+import events from './03-events/events.html';
+import bundles from './04-bundles/bundles.html';
+// import simpleEnemyBehavior from './03-simple-enemy-behavior/simple-enemy-behavior.html';
+
+const server = serve({
+	port: 3000,
+	development: true,
+	routes: {
+		'/': home,
+		'/movement/': basiceMovement,
+		'/player-input/': playerInput,
+		'/events/': events,
+		'/bundles/': bundles,
+		// '/simple-enemy-behavior': simpleEnemyBehavior
+	},
+});
+
+console.log(`Listening on ${server.hostname}:${server.port}`);
